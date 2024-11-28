8b653803634e6dbde0b16c1ab1de83db638d327a
unknown <pboyer.306@gmail.com>
2024-11-28 00:47:31 -0700

    more changes to get to run properly

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -5,35 +5,10 @@ console.log(AuthService.isTokenExpired());
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-  const response = await fetch("auth/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userInfo),
-  });
-
+  const response = await fetch("auth/login", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(userInfo),});
   return await response.json();
-};
+}
 
-const fetchTickets = async () => {
-  const token = localStorage.getItem('token');
 
-  if (!token || AuthService.isTokenExpired()) {
-    throw new Error("Token is invalid or expired. Please log in again.");
-  }
 
-  const response = await fetch("api/tickets", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch tickets");
-  }
-
-  return await response.json();
-};
-
-export { login, fetchTickets };
+export { login };
--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -5,23 +5,21 @@ interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction): void => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) {
-    res.status(401).json({ message: 'Access token is missing or invalid.' });
-    return;
-  }
-
-  try {
-    const secretKey = process.env.JWT_SECRET as string;
-    const payload = jwt.verify(token, secretKey) as JwtPayload;
-    (req as any).user = payload;
-    next();
-  } catch (err) {
-    res.status(403).json({ message: 'Invalid token.' });
-    return;
-  }
+  const authCombine = req.headers.authorization;
+  if (authCombine) {
+    const token = authCombine.split(" ")[1];
+    const sKey = process.env.JWT_SECRET_KEY || "";
+    jwt.verify(token, sKey, (err, user) => {
+        if (err) {
+            return res.status(401).json({ message: "error" });
+        }
+        req.user = user as JwtPayload;
+        return next();
+    });
+}
+else {
+    res.status(401);
+}
 };
